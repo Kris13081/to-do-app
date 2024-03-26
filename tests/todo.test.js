b149a579fc8c927e5e5896b95cc9fcf28eed9e1b
Kris13081 <kristianstefanov0@gmail.com>
2024-03-26 03:37:11 +0200

    delete task test done

--- tests/todo.test.js
+++ tests/todo.test.js
@@ -9,3 +9,15 @@ test('user can add a task', async ({ page }) => {
     const taskText = await page.textContent('.task');
     expect(taskText).toContain('Test Task')
 });
+
+test('user can delete a task', async ({ page }) => {
+    //Add task
+    await page.goto('http://localhost:5500/');
+    await page.fill('#task-input', 'Test Task');
+    await page.click('#add-task');
+    //Delete task
+    await page.click('.task .delete-task');
+    const tasks = await page.$$eval('.task',
+    tasks => tasks.map(task => task.textContent));
+    expect(tasks).not.toContain('Test Task');
+});
