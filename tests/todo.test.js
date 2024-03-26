dcff94c8536df88ea8210a9e2b37e3894b5d10bb
Kris13081 <kristianstefanov0@gmail.com>
2024-03-26 03:29:38 +0200

    fixed tests related bug

--- tests/todo.test.js
+++ tests/todo.test.js
@@ -3,7 +3,7 @@ const { test, expect } = require('@playwright/test');
 
 test('user can add a task', async ({ page }) => {
     
-    await page.goto('http://localhost:5000/');
+    await page.goto('http://localhost:5500/');
     await page.fill('#task-input', 'Test Task');
     await page.click('#add-task');
     const taskText = await page.textContent('.task');
